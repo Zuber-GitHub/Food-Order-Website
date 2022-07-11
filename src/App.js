77033f6fef387f0e38f56cfce2b628067403cb48
Zuber-GitHub <zuberriyazahmed@gmail.com>
2022-07-11 16:59:25 +0530

    MealForm Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
 import MealsSummary from './components/Meals/MealsSummary';
+import AvailableMeals from './components/Meals/AvailableMeals';
 
 function App() {
+  const dummyItems = [
+    {
+        dishName:'Sushi',
+        description:'Finest Fish and Vegies',
+        amount:'$29.99',
+        id: Math.random().toString()
+    },
+    {
+        dishName:'Dosa',
+        description:'Dharwad Speciality',
+        amount:'$10.99',
+        id: Math.random().toString()
+    },
+    {
+        dishName:'Idli',
+        description:'Begluru Speciality',
+        amount:'$9.99',
+        id: Math.random().toString()
+    },
+    {
+        dishName:'Biriyani',
+        description:'BVB Speciality',
+        amount:'$15.99',
+        id: Math.random().toString()
+    }
+]
   return (
     <Fragment>
       <Header />
       <MealsSummary/>
+      <AvailableMeals Items = {dummyItems}/>
     </Fragment>
   );
 }
